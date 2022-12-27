13ee1fb036e490abf8bf88bd69c0e53397e8a83b
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-12-27 17:56:14 +0100

    fix: no style flash during loading

--- web/libs/styles/src/lib/globalStyles.ts
+++ web/libs/styles/src/lib/globalStyles.ts
@@ -11,6 +11,7 @@ export const globalStyles = globalCss({
   ],
   "@font-face": [
     {
+      fontDisplay: "block",
       fontFamily: "Play",
       src: "url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&display=swap')",
     },
